e49b8110e0bfcffeab099154afe2ae30cc6c82ba
Dave Supplee <dwsupplee@gmail.com>
2017-01-04 12:10:18 -0500

    testing

--- appveyor_install_php.ps1
+++ appveyor_install_php.ps1
@@ -1,2 +1,6 @@
-appveyor DownloadFile http://windows.php.net/downloads/releases/php-$env:PHP_VERSION.zip -or appveyor DownloadFile http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION.zip
+try {
+    appveyor DownloadFile http://windows.php.net/downloads/releases/php-$env:PHP_VERSION.zip
+} catch {
+	appveyor DownloadFile http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION.zip
+}
 7z x php-$env:PHP_VERSION.zip -oc:\tools\php
