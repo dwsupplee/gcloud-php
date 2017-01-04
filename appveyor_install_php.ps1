b7436953636604252f0867a34a4fe2675ea9504f
Dave Supplee <dwsupplee@gmail.com>
2017-01-04 13:50:05 -0500

    testing

--- appveyor_install_php.ps1
+++ appveyor_install_php.ps1
@@ -1,13 +1,12 @@
 $file = "php-$env:PHP_VERSION.zip"
-$projectPath = "C:\tools\php"
 $url = "http://windows.php.net/downloads/releases/$file"
 $archiveUrl = "http://windows.php.net/downloads/releases/archives/$file"
 $client = New-Object NET.WebClient
 
 try {
-    $client.DownloadFile($url, "$projectPath\$file")
+    $client.DownloadFile($url, "C:\tools\php\$file")
 } catch {
-	$client.DownloadFile($archiveUrl, "$projectPath\$file")
+	$client.DownloadFile($archiveUrl, "C:\tools\php\$file")
 }
 
-7z x $file -oc:\tools\php
+7z x C:\tools\php\$file -oc:\tools\php
