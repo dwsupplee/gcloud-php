e5f0ff314515da286de4b8be48546ad32f7559ab
Dave Supplee <dwsupplee@gmail.com>
2017-01-05 11:39:55 -0500

    testing

--- appveyor_install_php.ps1
+++ appveyor_install_php.ps1
@@ -2,13 +2,12 @@ $file = "php-$env:PHP_VERSION.zip"
 $url = "http://windows.php.net/downloads/releases/$file"
 $archiveUrl = "http://windows.php.net/downloads/releases/archives/$file"
 $client = New-Object NET.WebClient
-$path = "C:\tools\php\$file"
+$projectPath = "C:\projects\google-cloud"
 
 try {
-appveyor DownloadFile http://windows.php.net/downloads/releases/archives/php-$env:PHP_VERSION.zip
-    $client.DownloadFile($url, "C:\projects\google-cloud\$file")
+    $client.DownloadFile($url, "$projectPath\$file")
 } catch {
-	$client.DownloadFile($archiveUrl, "C:\projects\google-cloud\$file")
+	$client.DownloadFile($archiveUrl, "$projectPath\$file")
 }
 
 7z x $file -oc:\tools\php
